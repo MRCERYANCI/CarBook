de6814d617e845c8a52be149ea3de8bcee86f7cc
burakcokkececi363@outlook.com <burakcokkececi363@outlook.com>
2024-05-20 14:02:06 +0300

    ...

--- Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
@@ -1,12 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.DtoLayer.Dtos.StatisticsDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.DefaultViewComponents
 {
 	public class _DefaultStatisticsComponentPartial : ViewComponent
 	{
-		public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
 		{
-			return View();
+            var client = _httpClientFactory.CreateClient();
+
+            #region GetCarCount
+            var responseGetCarCountMessage = await client.GetAsync("https://localhost:7125/api/Statistics/GetCarCount");
+            if (responseGetCarCountMessage.IsSuccessStatusCode)//200 ile 299 arasında bir sayı dönerse true döneceğinden başarılı false dönerse başarısız
+            {
+                var GetCarCountjsondata = await responseGetCarCountMessage.Content.ReadAsStringAsync();//Bu veri json trü
+                var GetCarCountvalues = JsonConvert.DeserializeObject<ResultStatisticsDto>(GetCarCountjsondata); //Json Türünü Normale Çevirmek için DeserializeObject Kullanılır
+                ViewBag.CarCount = GetCarCountvalues.carCount;
+            }
+            #endregion
+
+            #region GetLocationCount
+            var responseGetLocationCountMessage = await client.GetAsync("https://localhost:7125/api/Statistics/GetLocationCount");
+            if (responseGetLocationCountMessage.IsSuccessStatusCode)//200 ile 299 arasında bir sayı dönerse true döneceğinden başarılı false dönerse başarısız
+            {
+                var GetLocationCountjsondata = await responseGetLocationCountMessage.Content.ReadAsStringAsync();//Bu veri json trü
+                var GetLocationCountvalues = JsonConvert.DeserializeObject<ResultStatisticsDto>(GetLocationCountjsondata); //Json Türünü Normale Çevirmek için DeserializeObject Kullanılır
+                ViewBag.LocationCount = GetLocationCountvalues.locationCount;
+            }
+            #endregion
+
+            #region GetBrandCount
+            var responseGetBrandCountMessage = await client.GetAsync("https://localhost:7125/api/Statistics/GetBrandCount");
+            if (responseGetBrandCountMessage.IsSuccessStatusCode)//200 ile 299 arasında bir sayı dönerse true döneceğinden başarılı false dönerse başarısız
+            {
+                var GetBrandCountjsondata = await responseGetBrandCountMessage.Content.ReadAsStringAsync();//Bu veri json trü
+                var GetBrandCountvalues = JsonConvert.DeserializeObject<ResultStatisticsDto>(GetBrandCountjsondata); //Json Türünü Normale Çevirmek için DeserializeObject Kullanılır
+                ViewBag.BrandCount = GetBrandCountvalues.brandCount;
+            }
+            #endregion
+
+            #region GetCarCountByFuelElectric
+            var responseGetCarCountByFuelElectricMessage = await client.GetAsync("https://localhost:7125/api/Statistics/GetCarCountByFuelElectric");
+            if (responseGetCarCountByFuelElectricMessage.IsSuccessStatusCode)//200 ile 299 arasında bir sayı dönerse true döneceğinden başarılı false dönerse başarısız
+            {
+                var GetCarCountByFuelElectricjsondata = await responseGetCarCountByFuelElectricMessage.Content.ReadAsStringAsync();//Bu veri json trü
+                var GetCarCountByFuelElectricvalues = JsonConvert.DeserializeObject<ResultStatisticsDto>(GetCarCountByFuelElectricjsondata); //Json Türünü Normale Çevirmek için DeserializeObject Kullanılır
+                ViewBag.CarCountByFuelElectricvalues = GetCarCountByFuelElectricvalues.carCountByFuelElectric;
+            }
+            #endregion
+
+
+            return View();
 		}
 	}
 }
--- Frontends/CarBook.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,11 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Domain.Entities;
+using CarBook.DtoLayer.Dtos.BrandDtos;
+using CarBook.DtoLayer.Dtos.CarDtos;
+using CarBook.DtoLayer.Dtos.FooterAddressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _FooterUILayoutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var FooterAddressMessage = await client.GetAsync("https://localhost:7125/api/FooterAddress/4");
+            if (FooterAddressMessage.IsSuccessStatusCode)//200 ile 299 arasında bir sayı dönerse true döneceğinden başarılı false dönerse başarısız
+            {
+                var jsondata = await FooterAddressMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCarDto>(jsondata); //Json Türünü Normale Çevirmek için DeserializeObject Kullanılır
+
+                var FooterAddressjsondata = await FooterAddressMessage.Content.ReadAsStringAsync();
+                var FooterAddressvalues = JsonConvert.DeserializeObject<ResultFooterAddressDto>(FooterAddressjsondata);
+                
+                return View(FooterAddressvalues);
+            }
             return View();
         }
     }
